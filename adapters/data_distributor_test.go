4e41f5c8f4b162862b930cf19208055733b5a685
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-05-31 19:50:35 +0100

    add a test for data distributor adapter

---
+++ adapters/data_distributor_test.go
@@ -0,0 +1,33 @@
+package adapters
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDataDistributor(t *testing.T) {
+
+	t.Run("test with numbers passed to the distributor", func(t *testing.T) {
+		mockArrayOfFileNamesFromCLI := []string{}
+		mockArrayOfNumbersFromCLI := []string{"1,2,3,4"}
+		result := DataDistributor(mockArrayOfFileNamesFromCLI, mockArrayOfNumbersFromCLI)
+		expected := []int{1, 2, 3, 4}
+		assert.Equal(t, expected, result)
+	})
+	t.Run("test with file name passed to the distributor", func(t *testing.T) {
+		mockArrayOfFileNamesFromCLI := []string{"../testdata/test_numbers.csv"}
+		mockArrayOfNumbersFromCLI := []string{}
+		result := DataDistributor(mockArrayOfFileNamesFromCLI, mockArrayOfNumbersFromCLI)
+		expected := []int{1, 2, 32, 321, 1234, 4567890}
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("test with 2 file names passed to the distributor", func(t *testing.T) {
+		mockArrayOfFileNamesFromCLI := []string{"../testdata/test_numbers.csv", "../testdata/test_numbers.txt"}
+		mockArrayOfNumbersFromCLI := []string{}
+		result := DataDistributor(mockArrayOfFileNamesFromCLI, mockArrayOfNumbersFromCLI)
+		expected := []int{1, 2, 32, 321, 1234, 4567890, 33, 44, 55, 678}
+		assert.Equal(t, expected, result)
+	})
+}
