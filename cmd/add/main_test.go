f6c553c2e8e420b106260bea9fe24b1e8b57d298
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-06-07 09:45:06 +0100

    first attemt to test main file of add func

---
+++ cmd/add/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+var (
+	binName = "add"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+	build := exec.Command("go", "build", "-o", binName)
+
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests....")
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+
+	os.Exit(result)
+}
+
+//go run cmd/add/main.go --input-numbers="1,2,3"
+
+func TestCLI(t *testing.T) {
+	numbers := "1,2,3"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("Add number from CLI", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "--input-numbers", numbers)
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+}
