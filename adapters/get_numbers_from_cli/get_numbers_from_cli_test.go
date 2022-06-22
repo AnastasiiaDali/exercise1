b128880d0ed40c3edf5be483c2bab1be8377ce9f
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-06-22 20:25:41 +0100

    fix test

--- adapters/get_numbers_from_cli/get_numbers_from_cli_test.go
+++ adapters/get_numbers_from_cli/get_numbers_from_cli_test.go
@@ -1,15 +1,16 @@
-package get_numbers_from_cli
+package get_numbers_from_cli_test
 
 import (
 	"testing"
 
+	"exercise1/adapters/get_numbers_from_cli"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetNumbersFromCLI(t *testing.T) {
 	strOfNum := []string{"1,2,3"}
 	want := []int{1, 2, 3}
-	got := GetNumbersFromCLI(strOfNum)
+	got := get_numbers_from_cli.GetNumbersFromCLI(strOfNum)
 
 	assert.Equal(t, want, got)
 }
--- adapters/get_numbers_from_files/get_numbers_from_files_test.go
+++ adapters/get_numbers_from_files/get_numbers_from_files_test.go
@@ -9,8 +9,6 @@ import (
 
 //get number from the file function returns an array of integers extracted from the file.
 func TestGetNumbersFromFile(t *testing.T) {
-	t.Skip()
-
 	t.Run("should return array of integers from one file", func(t *testing.T) {
 		testArrayOfFileName1 := []string{"input1.csv"}
 
@@ -23,6 +21,7 @@ func TestGetNumbersFromFile(t *testing.T) {
 	//number from the file function should remove duplicated numbers
 
 	t.Run("should return array of integers from three file", func(t *testing.T) {
+		t.Skip()
 		testArrayOfFileName2 := []string{"/Users/anastasiia.dalakishvili/github/personal/exercise1/testdata/test_numbers.csv", "/Users/anastasiia.dalakishvili/github/personal/exercise1/testdata/test_numbers.txt", "/Users/anastasiia.dalakishvili/github/personal/exercise1/testdata/test_number2.txt"}
 
 		expected := []int{1, 2, 32, 321, 1234, 4567890, 33, 44, 55, 678}
