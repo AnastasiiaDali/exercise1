ab5d33486fa1d02dcd33b6f324247e1459c04543
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-06-22 20:15:25 +0100

    bringing tests back

--- helpers/string_to_int_converter/converter_string_into_integer_test.go
+++ helpers/string_to_int_converter/converter_string_into_integer_test.go
@@ -6,18 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//func TestDataConverter(t *testing.T) {
-//	t.Run("Converter should return an array of integers", func(t *testing.T) {
-//		data := "1,2,3"
-//		var desiredRes []int
-//		result := DataConverter(data)
-//
-//		assert.IsType(t, desiredRes, result)
-//	})
-//}
-
 func TestStringToIntConverter(t *testing.T) {
-	t.Skip()
 	t.Run("Converter should return an array of integers when given array of strings", func(t *testing.T) {
 		data := []string{"1", "2", "3"}
 		var desiredRes []int
