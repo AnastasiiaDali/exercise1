7724b23e769260f240f7c5559d8695a978d7a317
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-06-23 22:53:14 +0100

    uncomment the test

--- cmd/math/main_test.go
+++ cmd/math/main_test.go
@@ -1,50 +1,49 @@
 package main
 
-//
-//import (
-//	"fmt"
-//	"os"
-//	"os/exec"
-//	"path/filepath"
-//	"testing"
-//)
-//
-//var (
-//	binName = "math"
-//)
-//
-//func TestMain(m *testing.M) {
-//
-//	fmt.Println("Building tool...")
-//	build := exec.Command("go", "build", "-o", binName)
-//
-//	if err := build.Run(); err != nil {
-//		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
-//		os.Exit(1)
-//	}
-//
-//	fmt.Println("Running tests....")
-//	result := m.Run()
-//
-//	fmt.Println("Cleaning up...")
-//	os.Remove(binName)
-//
-//	os.Exit(result)
-//}
-//
-//func TestCLI(t *testing.T) {
-//	dir, err := os.Getwd()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	cmdPath := filepath.Join(dir, binName)
-//
-//	t.Run("start the server after passing correct flag", func(t *testing.T) {
-//		cmd := exec.Command(cmdPath, "--web-server")
-//
-//		if err := cmd.Run(); err != nil {
-//			t.Fatal(err)
-//		}
-//	})
-//}
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+var (
+	binName = "math"
+)
+
+func TestMain(m *testing.M) {
+
+	fmt.Println("Building tool...")
+	build := exec.Command("go", "build", "-o", binName)
+
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests....")
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+
+	os.Exit(result)
+}
+
+func TestCLI(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("start the server after passing correct flag", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "--web-server")
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
