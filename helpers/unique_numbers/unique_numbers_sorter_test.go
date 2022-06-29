9a49b95a8960b4ce02b87af1f51528259157a848
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-06-29 10:56:30 +0100

    refactor test

--- helpers/unique_numbers/unique_numbers_sorter_test.go
+++ helpers/unique_numbers/unique_numbers_sorter_test.go
@@ -7,9 +7,17 @@ import (
 )
 
 func TestUniqueNumbers(t *testing.T) {
-	unsortedNumber := []int{1, 2, 3, 4, 5, 5}
-	want := []int{1, 2, 3, 4, 5}
-	got := UniqueNumbers(unsortedNumber)
-
-	assert.Equal(t, want, got)
+	testCases := []struct {
+		input []int
+		want  []int
+	}{
+		{input: []int{1, 2, 3, 4, 5, 5}, want: []int{1, 2, 3, 4, 5}},
+		{input: []int{1, 1, 1}, want: []int{1}},
+	}
+	for _, tc := range testCases {
+		t.Run("given an array of number should only unique numbers", func(t *testing.T) {
+			result := UniqueNumbers(tc.input)
+			assert.Equal(t, tc.want, result)
+		})
+	}
 }
