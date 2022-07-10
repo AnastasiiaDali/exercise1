b48a3196e0989e630652815ff9ccd7633253374b
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-07-10 14:40:15 +0100

    refactor math program

---
+++ adapters/http/router.go
@@ -0,0 +1,62 @@
+package temphttp
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"exercise1/domain/formatter"
+	"exercise1/helpers/string_to_int_converter"
+)
+
+type TempCalculator interface {
+	Add(numbers []int) int
+}
+
+type server struct {
+	tempCalculator TempCalculator
+}
+
+func NewRouter(calculatorService TempCalculator) http.Handler {
+	svr := server{tempCalculator: calculatorService}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/add", svr.calculator)
+
+	return mux
+}
+
+func (s server) calculator(w http.ResponseWriter, r *http.Request) {
+	var numbersStr []string
+
+	//get numbers from url
+	num := r.URL.Query()
+	for _, v := range num {
+		numbersStr = v
+	}
+
+	//convert number into array of integers
+	numbers := string_to_int_converter.StringToIntConverter(numbersStr)
+
+	sum := s.tempCalculator.Add(numbers)
+	formatterSum := formatter.FormatNumber(sum)
+
+	res, err := json.Marshal(formatterSum)
+
+	w.Header().Add("Content-type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+}
+
+//func MathServer() {
+//	r := mux.NewRouter()
+//	r.HandleFunc("/", HealthCheckHandler).Methods(http.MethodGet)
+//	r.HandleFunc("/add", MathHandler).Methods(http.MethodPost)
+//
+//	fmt.Printf("Starting server on port 8081...\n")
+//	http.ListenAndServe(":8081", r)
+//}
--- cmd/math/main.go
+++ cmd/math/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
 
 	"exercise1/adapters/http"
+	calculator "exercise1/domain/calculator"
 )
 
 func main() {
 	startServer := os.Args[1]
 
+	calculator := calculator.New()
+
+	router := temphttp.NewRouter(calculator)
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+
 	if startServer == "--web-server" {
-		http.MathServer()
+		err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else {
 		panic("ops, seems like the flag is wrong. try --web-server")
 	}
