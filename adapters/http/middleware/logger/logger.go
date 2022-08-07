286c286cf20bb219b76bf583a2c81af575886e49
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-08-07 13:42:16 +0100

    log first 8 character of auth key

--- adapters/http/middleware/logger/logger.go
+++ adapters/http/middleware/logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type LoggingMiddleware struct {
@@ -14,7 +15,10 @@ func NewAuthMiddleware() LoggingMiddleware {
 
 func (m *LoggingMiddleware) AuthHandler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.Method, r.URL, r.Body)
+		authorizationKey := r.Header.Get("Authorization")
+		key := strings.Replace(authorizationKey, "Bearer", "", 1)
+		cutKey := key[0:9]
+		log.Printf("%s %s %s %s\n", r.Method, r.URL, r.Body, cutKey)
 		handler.ServeHTTP(w, r)
 	})
 }
